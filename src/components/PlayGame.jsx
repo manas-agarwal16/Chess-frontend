8582a5040257388a3c56f34b74d039d5b83a85cf
manas-agarwal16 <manas.agarwal1604@gmail.com>
2025-01-11 22:59:37 +0530

    Valid moves are working fine

--- src/components/PlayGame.jsx
+++ src/components/PlayGame.jsx
@@ -20,6 +20,7 @@ const PlayGame = () => {
   const dispatch = useDispatch();
 
   const [game, setGame] = useState(new Chess());
+  const [position, setPosition] = useState(game.fen());
 
   const { loginStatus, playerData, loading } = useSelector(
     (state) => state.auth
@@ -30,6 +31,7 @@ const PlayGame = () => {
   const [color, setColor] = useState(null);
   const [you, setYou] = useState({});
   const [opponent, setOpponent] = useState({});
+  const [roomName, setRoomName] = useState(null);
 
   const [isProcessOngoing, setIsProcessOngoing] = useState(true);
 
@@ -104,6 +106,7 @@ const PlayGame = () => {
     console.log("player1 : ", players.player1);
     console.log("player2 : ", players.player2);
 
+    setRoomName(players.roomName);
     setIsInGame(true);
     setColor(
       players.player1.id === playerData.id
@@ -123,43 +126,77 @@ const PlayGame = () => {
     // navigate("/play");
   });
 
+  // const onDrop = (sourceSquare, targetSquare, piece) => {
+  //   console.log("sourceSquare : ", sourceSquare);
+  //   console.log("targetSquare : ", targetSquare);
+  //   console.log("piece : ", piece);
+
+  //   const gameCopy = new Chess(game.fen());
+
+  //   const move = gameCopy.move({
+  //     from: sourceSquare,
+  //     to: targetSquare,
+  //     promotion: game._turn == "w" ? "Q" : "q", // always promote to a queen for example simplicity
+  //   });
+
+  //   if (move === null) {
+  //     console.log("invalid move");
+  //     return;
+  //   }
+  //   socket.emit("newChessPosition", { position: gameCopy.fen(), roomName });
+  //   socket.on("makeMove", (newPosition) => {
+  //     console.log("new position : ", newPosition);
+  //     setPosition(newPosition);
+  //     setGame(new Chess(newPosition));
+  //   });
+  // };
+
   const onDrop = (sourceSquare, targetSquare, piece) => {
     console.log("sourceSquare : ", sourceSquare);
     console.log("targetSquare : ", targetSquare);
     console.log("piece : ", piece);
 
-    console.log("game : ", game);
-
-    const move = game.move({
+    const gameCopy = new Chess(game.fen());
+    console.log("game : ", gameCopy.fen());
+    const move = gameCopy.move({
       from: sourceSquare,
       to: targetSquare,
-      promotion: game._turn == "w" ? "Q" : "q", // always promote to a queen for example simplicity
+      // promotion: gameCopy._turn == "w" ? "Q" : "q", // always promote to a queen for example simplicity
     });
+
+    console.log("move : ", move);
 
     if (move === null) {
       console.log("invalid move");
-      return;
+      setGame(game);
+      return false;
     }
-    setGame(new Chess(game.fen()));
 
-    socket.emit("newChessPosition", gameCopy);
-    // socket.on("makeMove", () => {
-    //   setGame(chess.fen());
-    // });
+    socket.emit("newChessPosition", { position: gameCopy.fen(), roomName });
+    return true;
   };
+  socket.on("makeMove", (newPosition) => {
+    console.log("new position : ", newPosition);
+    setPosition(newPosition);
+    setGame(new Chess(newPosition));
+  });
 
   return (
     <div className="h-screen w-screen flex flex-col items-center justify-center">
       {loading && <CenterSpinner />}
       {isGameLoading && <p>Waiting for a player to join</p>}
       {!loading && !isGameLoading && (
-        <div className="w-[350px] h-[350px] lg:w-[450px] lg:h-[450px] xl:w-[550px] xl:h-[550px] p-4 mx-auto border-2 border-red-600">
+        <div className="w-full max-w-[550px] p-4 mx-auto">
           <Chessboard
             id="PlayVsRandom"
-            position={game.fen()}
+            position={position}
             onPieceDrop={onDrop}
             boardOrientation={color}
-            className="w-full h-full"
+            customBoardStyle={{
+              borderRadius: "4px",
+              boxShadow: "0 2px 10px rgba(0, 0, 0, 0.5)",
+              width: "100%", // Ensure it takes full width of the parent container
+            }}
           />
         </div>
       )}
