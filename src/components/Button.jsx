9c4fa9a6acae4fb547e2e851c58423762fa32b4a
manas-agarwal16 <manas.agarwal1604@gmail.com>
2025-02-01 16:07:13 +0530

    backendURL change

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,7 +5,7 @@ const Button = ({ text, className, bgColor = 'bg-[#F5DEB3]',width="w-96", ...pro
     <div>
       <button
         {...props}
-        className={`hover:scale-105 rounded-lg px-4 py-2 text-3xl font-sans transition-transform duration-900 italic text-gray-700 ${className} ${bgColor} ${width}`}
+        className={`hover:scale-105 rounded-lg px-4 py-2 text-3xl font-sans transition-transform duration-900 italic text-gray-700 ${className} ${bgColor} ${width}`} 
       >
         {text}
       </button>
