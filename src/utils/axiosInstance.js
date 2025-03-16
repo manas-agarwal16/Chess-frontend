06042db2606c26245c6f38864e428356912ed06c
manas-agarwal16 <manas.agarwal1604@gmail.com>
2025-03-16 18:14:11 +0530

    backend url changed

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -3,7 +3,7 @@ const BACKENDURL = import.meta.env.VITE_BACKEND_URL;
 console.log("BACKENDURL : ", BACKENDURL); 
 
 
-const axiosInstance = axios.create();
+const axiosInstance = axios.create(); 
 
 axiosInstance.defaults.baseURL = BACKENDURL;
 axiosInstance.defaults.withCredentials = true;
